f34967f57b76f1239b4a79db049b102d5cc33c3c
tuzliga <adiletdzoldosbekov5199@gmail.com>
2026-05-16 23:53:15 +0600

    Update app.js

--- app.js
+++ app.js
@@ -285,8 +285,8 @@ html += `
     }"
 
     style="
-      width:36px;
-      height:36px;
+      width:42px;
+      height:42px;
       object-fit:contain;
       flex-shrink:0;
     "
@@ -383,7 +383,7 @@ html += `
 
   <div style="
     color:white;
-    font-size:12px;
+    font-size:16px;
     font-weight:500;
   ">
 
@@ -406,8 +406,8 @@ html += `
     }"
 
     style="
-      width:36px;
-      height:36px;
+      width:42px;
+      height:42px;
       object-fit:contain;
       flex-shrink:0;
     "
