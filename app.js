7b11cffe2096130a069b287616ae2b5a10db705e
tuzliga <adiletdzoldosbekov5199@gmail.com>
2026-05-18 11:19:02 +0600

    Update app.js

--- app.js
+++ app.js
@@ -192,48 +192,38 @@ if (modeTabs) {
   let html = "";;
   // SCORERS SCREEN
 
-const photoMap = {};
+if (currentTab === "scorers") {
 
-photosData.forEach(player => {
+  const topScorers = [
+    {
+      name:"Эрнист Болотбеков",
+      team:"1996",
+      goals:25,
+      photo:"./assets/player1.png"
+    },
+    {
+      name:"Мирлан Уулу",
+      team:"1994",
+      goals:19,
+    photo:"./assets/player2.png"
+    },
+    {
+      name:"Бексултан Абдиев",
+      team:"00-01",
+      goals:16
+    },
+    {
+      name:"Айдарбек Молдокулов",
+      team:"2006",
+      goals:14
+    },
+    {
+      name:"Нурболот Мурзаев",
+      team:"УЛУУ МУУН",
+      goals:11
+    }
+  ];
 
-  const playerName =
-    (player["ИГРОК"] || "")
-    .trim();
-
-  const photo =
-    (player["ФОТО"] || "")
-    .trim();
-
-  if (playerName) {
-
-    photoMap[playerName] =
-      photo;
-  }
-});
-
-const topScorers =
-  scorersData
-    .slice(0, 5)
-    .map(player => ({
-
-      name:
-        player["ИГРОК"],
-
-      team:
-        player["КОМАНДА"],
-
-      goals:
-        Number(
-          player["ГОЛЫ"]
-        ) || 0,
-
-      photo:
-        photoMap[
-          player["ИГРОК"]
-        ]
-        ||
-        "./assets/defaultplayer.png"
-    }));
   html += `
     <div class="
       scorers-wrap
@@ -261,11 +251,10 @@ const topScorers =
     "./assets/1990.png";
 }
 
-    cconst avatar =
-  player.photo &&
-  player.photo.trim()
-    ? player.photo
-    : "./assets/defaultplayer.png";
+    const avatar =
+      player.photo ||
+      "./assets/defaultplayer.png";
+
     html += `
       <div class="
         scorer-card
